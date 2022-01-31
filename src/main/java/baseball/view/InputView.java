a7c045f6e4d7e0de18aeda8f54604d9b9c8b1980
Yunkeun <yunx2@naver.com>
2022-02-01 02:29:51 +0900

    fix: test 통과하게 예외 처리 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class InputView {
 
 	private static final String REGEX = "";
+	private static final String NUMBER_REGEX = "^[1-9]+$";
 	private static final int RESTART = 1;
 	private static final int END = 2;
 
@@ -24,6 +25,9 @@ public class InputView {
 
 	public static List<String> inputPlayerNumber() {
 		String inputNumber = Console.readLine();
+		if (!inputNumber.matches(NUMBER_REGEX) || inputNumber.isEmpty()) {
+			OutputView.printException();
+		}
 		List<String> playerNumber = Arrays.asList(inputNumber.split(REGEX));
 		if (playerNumber.get(0).equals(playerNumber.get(1))
 			|| playerNumber.get(1).equals(playerNumber.get(2))
